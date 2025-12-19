300dfc76d54fcaee11333cec86626774a5f99701
seatosky2002 <seatosky2002@gmail.com>
2025-12-20 00:36:28 +0900

    modified CORS

--- app/main.py
+++ app/main.py
@@ -1,17 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.core.scheduler import start_scheduler
 from app.api.v1.analytics import router as analytics_router
 from contextlib import asynccontextmanager
 from app.core.scheduler import start_scheduler, shutdown_scheduler
-
-app = FastAPI(
-    title="HowMuch API",
-    description="중고 애플 제품 시세 조회 API",
-    version="1.0.0",
-    docs_url="/docs",
-    redoc_url="/redoc"
-)
 
 
 @asynccontextmanager
@@ -22,11 +13,20 @@ async def lifespan(app: FastAPI):
     finally:
         shutdown_scheduler()
 
-app = FastAPI(lifespan=lifespan)
+app = FastAPI(
+    title="HowMuch API",
+    description="중고 애플 제품 시세 조회 API",
+    version="1.0.0",
+    docs_url="/docs",
+    redoc_url="/redoc",
+    lifespan=lifespan
+)
 
 origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "http://howmuchapple.store",
+    "https://howmuchapple.store",
 ]
 
 app.add_middleware(
